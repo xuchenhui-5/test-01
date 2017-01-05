4d71e176376cd33cdd05ecb499a9e8bdab552200
许晨辉 <xuchenhui@xuchenhuideMacBook-Pro.local>
2017-01-05 21:57:37 +0800

    Initial Commit

---
+++ test-01/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test-01
+//
+//  Created by 许晨辉 on 2017/1/5.
+//  Copyright © 2017年 许晨辉. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
